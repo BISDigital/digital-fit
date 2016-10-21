765f4b070e8df03d36034b990dc744912f7cac92
the-daniel-rothig <daniel.rothig@gmail.com>
2016-10-21 18:10:21 +0100

    initial commit of digital-fit

---
+++ app/models/Question.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.Map;
+
+/**
+ * Created by daniel.rothig on 21/10/2016.
+ *
+ * A single survey question
+ */
+public class Question {
+    public final int id;
+    public final String question;
+    public final Map<DigitalRole, Double> roleMap;
+
+
+    public Question(int id, String question, Map<DigitalRole, Double> roleMap) {
+        this.id = id;
+        this.question = question;
+        this.roleMap = roleMap;
+    }
+}
